99f0bdf1695d157c626c998c72f9df3b47fe54b3
Delaney Burke <delaney@vidtreone.com>
2023-12-09 02:10:57 +0200

    release 0.0.1
    The initial release of how the system manages port

---
+++ lib/portr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "portr/version"
+
+module Portr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/portr/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Portr
+  VERSION = "0.0.1"
+end
